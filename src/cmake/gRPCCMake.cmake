a332a4495abfbd63682808594263bbfaede44fcf
Yadong Qi <yadong.qi@intel.com>
2022-05-13 13:08:18 +0800

    5/13/2022
    Signed-off-by: Yadong Qi <yadong.qi@intel.com>

--- src/cmake/gRPCCMake.cmake
+++ src/cmake/gRPCCMake.cmake
@@ -7,10 +7,45 @@
 #
 
 include(FetchContent)
+
+macro(apply_git_patch REPO_PATH PATCH_PATH)
+  execute_process(COMMAND git apply --check ${PATCH_PATH}
+    WORKING_DIRECTORY ${REPO_PATH}
+    RESULT_VARIABLE SUCCESS
+    ERROR_QUIET)
+
+  if(${SUCCESS} EQUAL 0)
+    message("Applying git patch ${PATCH_PATH} in ${REPO_PATH} repository")
+    execute_process(COMMAND git apply ${PATCH_PATH}
+      WORKING_DIRECTORY ${REPO_PATH}
+      RESULT_VARIABLE SUCCESS)
+
+    if(${SUCCESS} EQUAL 1)
+      # We don't stop here because it can happen in case of parallel builds
+      message(WARNING "\nError: failed to apply the patch patch: ${PATCH_PATH}\n")
+    endif()
+  else()
+    message("Already applied: ${PATCH_PATH}")
+  endif()
+endmacro()
+
 FetchContent_Declare(
-  gRPC
+  grpc
   GIT_REPOSITORY https://github.com/grpc/grpc
-  GIT_TAG        v1.46.1
+  GIT_TAG        v1.43.0
 )
 set(FETCHCONTENT_QUIET OFF)
-FetchContent_MakeAvailable(gRPC)
+
+FetchContent_MakeAvailable(grpc)
+
+#Apply VSOCK patches
+file(GLOB patches_files "${CMAKE_SOURCE_DIR}/src/external/patches/grpc/*.patch")
+foreach(file ${patches_files})
+  apply_git_patch(${grpc_SOURCE_DIR} ${file})
+endforeach()
+
+set(_PROTOBUF_LIBPROTOBUF libprotobuf)
+set(_REFLECTION grpc++_reflection)
+set(_PROTOBUF_PROTOC $<TARGET_FILE:protoc>)
+set(_GRPC_GRPCPP grpc++)
+set(_GRPC_CPP_PLUGIN_EXECUTABLE  $<TARGET_FILE:grpc_cpp_plugin>)
--- src/guest/startup_listener.cc
+++ src/guest/startup_listener.cc
@@ -6,46 +6,66 @@
  *
  */
 
-#if 0
+
 #include <grpcpp/grpcpp.h>
 
-class StartupListenerImpl final : public StartupListener::Service {
-    public:
-        StartupListenerImpl() = default;
-        StartupListenerImpl(const StartupListenerImpl &) = delete;
-        StartupListenerImpl& operator=(const StartupListenerImpl&) = delete;
+#include "utils/log.h"
+#include "guest/startup_listener.h"
+#include "services/protos/gens/vm_host.grpc.pb.h"
 
-        ~StartupListenerImpl() override = default;
+namespace vm_manager {
 
-        grpc::Status VmReady(grpc::ServerContext* ctx, const EmptyMessage* request, EmptyMessage* respond) override;
+class StartupListenerImpl final : public vm_manager::StartupListener::Service {
+ public:
+    StartupListenerImpl() = default;
+    StartupListenerImpl(const StartupListenerImpl &) = delete;
+    StartupListenerImpl& operator=(const StartupListenerImpl&) = delete;
 
-        void AddPendingVM(uint32_t cid, base::WaitableEvent *event);
-        void RemovePendingVM(uint32_t cid);
+    ~StartupListenerImpl() override = default;
 
-    private:
-        std::map<uint32_t, std::function<void()>> pending_vms_;
-        base::Lock vm_lock_;
+    grpc::Status VmReady(grpc::ServerContext* ctx, const EmptyMessage* request, EmptyMessage* respond) override;
+
+    void AddPendingVM(uint32_t cid, std::function<void(void)> event);  //base::WaitableEvent *event);
+    void RemovePendingVM(uint32_t cid);
+
+ private:
+    std::map<uint32_t, std::function<void(void)>> pending_vms_;
+    std::mutex vm_lock_;
 };
 
-grpc::Status VmReady(grpc::ServerContext* ctx, const EmptyMessage* request, EmptyMessage* respond) {
+grpc::Status StartupListenerImpl::VmReady(grpc::ServerContext* ctx, const EmptyMessage* request, EmptyMessage* respond) {
     uint32_t cid = 0;
-    if (sscanf(ctx->peer().c_str(), "vsock:%" PRIu32, &cid) != 1) {
-        LOG(WARNING) << "Failed to parse peer vsock address " << ctx->peer();
+    if (sscanf(ctx->peer().c_str(), "vsock:%u", &cid) != 1) {
+        LOG(warning) << "Failed to parse peer vsock address " << ctx->peer();
         return grpc::Status(grpc::FAILED_PRECONDITION, "Invalid peer for StartupListener");
     }
 
-    base::AutoLock lock(vm_lock_);
+    std::scoped_lock lock(vm_lock_);
     auto iter = pending_vms_.find(cid);
     if (iter == pending_vms_.end()) {
-        LOG(ERROR) << "Received VmReady from VM with unknown cid: " << cid;
+        LOG(error) << "Received VmReady from VM with unknown cid: " << cid;
         return grpc::Status(grpc::FAILED_PRECONDITION, "VM is unknown");
     }
 
     //iter->second->Signal();
+    iter->second();
     pending_vms_[cid]();
 
     pending_vms_.erase(iter);
 
-    return grpc::Status:OK;
+    return grpc::Status::OK;
 }
-#endif
+
+void StartupListenerImpl::AddPendingVM(uint32_t cid, std::function<void(void)> event) {
+  std::scoped_lock lock(vm_lock_);
+
+  pending_vms_[cid] = event;
+}
+
+void StartupListenerImpl::RemovePendingVM(uint32_t cid) {
+  std::scoped_lock lock(vm_lock_);
+
+  pending_vms_.erase(cid);
+}
+
+}  // namespace vm_manager
